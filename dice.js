609a84f6f68f9dea9dc9799eb4751a56dee39bcf
Ilkenaal <vebjorrs@ifi.uio.no>
2017-08-22 20:03:42 +0200

    Extended the dice rolling functionality to allow for several bonuses and penalties in any order.

--- dice.js
+++ dice.js
@@ -2,86 +2,95 @@ var config = require("./config.json");
 
 var exports = module.exports = {};
 
-exports.getRandom = function (max) {
-	return Math.floor(Math.random() * max) + 1;
-};
-
 exports.parseRoll = function(input) {
 	var diceRegex = new RegExp(config.dice, 'g');
-	var negateRegex = new RegExp(config.negate);
 
-	var penalty = negateRegex.test(input);
-	var rolls = [];
+	var results = [];
 	var m;
 
 	console.log("input: " + input);
 
 	while (m = diceRegex.exec(input)) {
-		rolls.push(m);
-	}
-
-	console.log("Rolls discovered: " + rolls);
-	var results = []
-
-	for (var i = 0; i < rolls.length; i++) {
-		var currentRoll = this.rollDice(rolls[i]);
-		results.push(currentRoll);
+		results.push(rollDice(m));
 	}
 
 	return results;
 }
 
-exports.formatRoll = function(results, penalty) {
+exports.formatRoll = function(results) {
 	var sum = 0;
 	var response = "";
 	
 	for (var i = 0; i < results.length; i++) {
-
 		if (!(results[i].constructor === Array)) {
-			if (penalty) {
-				sum -= results[i];
+			let stringResult = String(results[i]);
+			if (stringResult === '+' || stringResult === '-') {
+				response += "    " + stringResult + "    ";
 			} else {
-				sum += results[i];
+				response += stringResult;
 			}
-					
-			response = response + results[i];
+
+			continue;
 		}
+		
 		for (var j = 0; j < results[i].length; j++) {
 			if (j == results[i].length - 1) {
 				response = response + "[ **" + results[i][j] + "** ]";
 			} else {
 				response = response + "[ **" + results[i][j] + "** ] + ";
 			}
-
-			sum += results[i][j];
-		}
-			
-		if (i < results.length - 1) {
-			if (penalty && (i == results.length - 2)) {
-				response += "    -    ";
-			} else {
-				response += "    +    ";
-			}
 		}
 	}
 
-	response = response + "    =    ***" + sum + "***";
+	response = response + "    =    ***" + getRollSum(results) + "***";
 
 	return response;
 };
 
-exports.rollDice = function (input) {
-	if (!String(input).includes('d')) {
+function getRollSum(input) {
+	var sum = 0;
+	let prevSymbol = "";
+	
+	for (let i = 0; i < input.length; i++) {
+		if (input[i] === '+' || input[i] === '-') {
+			prevSymbol = input[i];
+		} else if (prevSymbol === "" || prevSymbol === '+') {
+			sum += getArraySum(input[i]);
+		} else {
+			sum -= getArraySum(input[i]);
+		}
+	}
+	
+	return sum;
+}
+
+function getArraySum(input) {
+	if (input.constructor === Array) {
+		return input.reduce(function(a, b) { return a + b; }, 0);
+	} else {
+		return input;
+	}
+}
+
+function rollDice(input) {
+	let stringInput = String(input);
+
+	if (stringInput === '+' || stringInput === '-') {
+		return stringInput;
+	}
+	if (!stringInput.includes('d')) {
 		return parseInt(input);
 	}
 
-	var numbers = String(input).split("d");
+	var numbers = stringInput.split("d");
 	var results = []
 
 	for (var i = 0; i < numbers[0]; i++) {
-		results.push(this.getRandom(numbers[1]));
+		results.push(getRandom(numbers[1]));
 	}
 	return results;
-};
+}
 
-
+function getRandom(max) {
+	return Math.floor(Math.random() * max) + 1;
+}
